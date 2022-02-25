89f62f97b310b980a47903c81d97837081409a30
LikeaBillion <tombola11!>
2022-02-25 18:24:47 +0000

    Made big changes to the serial code. Needed to change the way i was handling grey images so this needed to be updated

--- Paralell/Paralell.cpp
+++ Paralell/Paralell.cpp
@@ -5,18 +5,20 @@
 #include <filesystem>
 #include <math.h>
 #include <filesystem>
-#include <algorithm>
-#include <execution>
+
+
 
 using namespace cv;
 using namespace std;
 using namespace chrono;
 
-
-//structure for storing images
+//structure for storing images- test, training and the distences
 struct Images {
     string label;
+    string test_name;
     Mat train_img;
+    Mat test_img;
+    Mat grey_img;
     double distance;
 
     ~Images() {};
@@ -25,7 +27,7 @@ struct Images {
 
 };
 
-//simple template for vectors
+//simple template for printing vectors
 template <typename T>
 void t_print(vector<T>& v) {
     if (v.empty()) return;
@@ -37,20 +39,14 @@ void t_print(vector<T>& v) {
 int counter = 0;
 ostream& operator<<(ostream& out, const Images& r) {
     counter++;
-    out << "{ counter: " << counter << " label: " << r.label << " distence: " << r.distance << " }" << endl;
+    out << "{ counter: " << counter << " label: " << r.label << " file name: " << r.test_name << " distence: " << r.distance << " }" << endl;
     return out;
 }
 
-//function to sort the distences when in the Image format
-bool compare_distance(Images a, Images b) {
-    if (a.distance < b.distance) {
-        return true;
-    } return false;
-}
-
 //serial implemntation of grayscaling
-void convert_to_grayscale_serial(unsigned char* input, unsigned char* output, int start, int end,
+void convert_to_grayscale(unsigned char* input, unsigned char* output, int start, int end,
     int channel) {
+    //j is need to insure the output is ordered right.
     int j = start;
     int number_of_pixel = end;
 
@@ -58,50 +54,32 @@ void convert_to_grayscale_serial(unsigned char* input, unsigned char* output, in
         int blue_value = input[i];
         int green_value = input[i + 1];
         int red_value = input[i + 2];
-
+        //shifts the pixels and then moves them into the output
         output[j++] = (int)(0.114 * blue_value + 0.587 * green_value + 0.299 * red_value);
     }
 }
-//function to calculate greyscale for the train_images then put them into a vector 
-unsigned char* calculate_grey_scale_train(vector<Images>& images, int value) {
-    unsigned char* input = (unsigned char*)images[value].train_img.data;
-    unsigned char* output = new unsigned char[images[value].train_img.size().width * images[value].train_img.size().height];
-    const int total_number_of_pixels = images[value].train_img.rows * images[value].train_img.cols * images[value].train_img.channels();
-    convert_to_grayscale_serial(input, output, 0, total_number_of_pixels, 3);
-    return output;
-}
-
-//function to calculate greyscale for the test_images then put them into a vector 
-unsigned char* calculate_grey_scale_test(vector<Mat>& images, int value) {
-    unsigned char* input = (unsigned char*)images[value].data;
-    unsigned char* output = new unsigned char[images[value].size().width * images[value].size().height];
-    const int total_number_of_pixels = images[value].rows * images[value].cols * images[value].channels();
-    convert_to_grayscale_serial(input, output, 0, total_number_of_pixels, 3);
-    return output;
-}
 
 //function to calcualte the euclidian distance between the images
-void calculate_distance(unsigned char* testimg, unsigned char* trainimg, int start, int end, vector<Images>& distance, string label) {
-    Images img;
-    double sum = 0;
+double calculate_distance(unsigned char* testimg, unsigned char* trainimg, int start, int end) {
+    double total = 0;
     for (int i = start; i < end; i++) {
-        sum = +pow((testimg[i] - trainimg[i]), 2);
+        total = +pow((testimg[i] - trainimg[i]), 2);
     }
-    double dis = sqrt(sum);
-    img.distance = dis;
-    img.label = label;
-    distance.push_back(img);
+    double dis = sqrt(total);
+    return dis;
 }
 
 //function to calculate the knn of the images- simply counts them and then prints the most common
-void calculate_knn(vector<Images>& distance, int kvalue) {
+void calculate_knn(vector<Images> distance, int kvalue) {
+    //variables to hold the counts for all the different types of images to sort
     double daisy_count = 0;
     double dandelion_count = 0;
     double rose_count = 0;
     double sunflower_count = 0;
     double tulip_count = 0;
+    int i;
 
-    for (int i = 0; i < kvalue; i++) {
+    for (i = 0; i < kvalue; i++) {
         if (distance[i].label == "daisy") {
             daisy_count++;
         }
@@ -120,25 +98,26 @@ void calculate_knn(vector<Images>& distance, int kvalue) {
 
     }
     cout << "----------------------------------------------------------" << endl;
+    cout << "Filename: " << distance[i].test_name << endl;
     cout << "Daisy with: " << daisy_count << " counts" << endl;
     cout << "Dandelion with: " << dandelion_count << " counts" << endl;
     cout << "Rose with: " << rose_count << " counts" << endl;
     cout << "Sunflower with: " << sunflower_count << " counts" << endl;
     cout << "Tulip with: " << tulip_count << " counts" << endl;
     if (max({ daisy_count,dandelion_count, rose_count, sunflower_count, tulip_count }) == daisy_count) {
-        cout << "Daisy with: " << 100 * (daisy_count / kvalue) << endl;
+        cout << "Its a daisy with: " << 100 * (daisy_count / kvalue) << " confidence" << endl;
     }
     else if (max({ daisy_count,dandelion_count, rose_count, sunflower_count, tulip_count }) == dandelion_count) {
-        cout << "Dandelion with: " << 100 * (dandelion_count / kvalue) << endl;
+        cout << "Its a dandelion with: " << 100 * (dandelion_count / kvalue) << " confidence" << endl;
     }
     else if (max({ daisy_count,dandelion_count, rose_count, sunflower_count, tulip_count }) == rose_count) {
-        cout << "Rose with: " << 100 * (rose_count / kvalue) << endl;
+        cout << "Its a rose with: " << 100 * (rose_count / kvalue) << " confidence" << endl;
     }
     else if (max({ daisy_count,dandelion_count, rose_count, sunflower_count, tulip_count }) == sunflower_count) {
-        cout << "Sunflower with: " << 100 * (sunflower_count / kvalue) << endl;
+        cout << "Its a sunflower with: " << 100 * (sunflower_count / kvalue) << " confidence" << endl;
     }
     else if (max({ daisy_count,dandelion_count, rose_count, sunflower_count, tulip_count }) == tulip_count) {
-        cout << "Tulip with: " << 100 * (tulip_count / kvalue) << endl;
+        cout << "Its a tulip with: " << 100 * (tulip_count / kvalue) << " confidence" << endl;
     }
 
 }
@@ -155,6 +134,19 @@ string calculate_label(string& dir) {
     }
     return directory;
 }
+//function to extract the filename from the path fo the test images
+string calculate_filename(string& dir) {
+    string name;
+    size_t last_slash_idx = dir.rfind('\\');
+    size_t end = dir.size();
+    if (std::string::npos != last_slash_idx)
+    {
+        name = dir.substr(last_slash_idx + 1, end);
+
+    }
+    return name;
+}
+
 //function to work out the subfolders from the path provideded by the command line 
 void find_subfolders(string dir, vector<string>& subfolders) {
     int label_counter = 0;
@@ -165,74 +157,122 @@ void find_subfolders(string dir, vector<string>& subfolders) {
 
 }
 
+
+
+
 int main(int argc, char** argv)
 {
-    std::cout << "starting the opencv basic.cpp...." << std::endl;
+    std::cout << "starting opencv...." << std::endl;
     //validating arguments
-    if (argc != 2) {
+    if (argc != 3) {
         cout << "Error in arguments..\n";
         exit(-1);
     }
 
-    //intiallizing vectors for image, filenames(train image), subfolders and test_img filenames and astring for the path of the test images
+    //storing the passed arguments for directory and k-values
     string dir(argv[1]);
-    Images img;
-    vector<Images> image;
-    vector<String> filenames;
-    vector<String> sub_folders;
-    vector<String> test_img_filenames;
-    vector<Mat> test_imgs;
+    string k_value(argv[2]);
+    //string to hold the test_img entered using the cin
     string test_img_path;
 
+    //image vectors for testing, training and distance data
+    vector<Images> test_image_data;
+    vector<Images> train_image_data;
+    vector<Images> distance_data;
+
+    //vectors to store filename sand test_filenames too
+    vector<String> filenames;
+    vector<String> test_img_filenames;
+
+    //vector to hold all the subfolders from the main directory
+    vector<String> sub_folders;
+
+    //staring timing
     auto start = steady_clock::now();
 
+    //cin the test_img path
     cout << "Input test directory: " << endl;
     cin >> test_img_path;
 
     //function calls to find all the subfolders
+    Images train_img;
     find_subfolders(dir, ref(sub_folders));
-    int read_count = 0;
     //iterates through all the subfolders adding to the image vector with all the training data and their labels
     for (int i = 0; i < sub_folders.size(); i++) {
         glob(sub_folders[i], filenames);
         for (int j = 0; j < filenames.size(); j++) {
-            img.train_img = (imread(filenames[j]));
-            img.label = calculate_label(filenames[j]);
-            image.push_back(img);
-            read_count++;
+            train_img.train_img = (imread(filenames[j]));
+            train_img.label = calculate_label(filenames[j]);
+            train_image_data.push_back(train_img);
         }
     }
-    int read_count_test = 0;
-    //interates through all the test_imgs add puts them into the image vector
+
+    //interates through all the test_imgs add puts them into the image vector. Along with the test_name to be used later
+    Images test_img;
     glob(test_img_path, test_img_filenames);
-    for (size_t i = 0; i < test_img_filenames.size(); i++) {
-        test_imgs.push_back(imread(test_img_filenames[i]));
-        read_count_test++;
+    for (int i = 0; i < test_img_filenames.size(); i++) {
+        test_img.test_img = (imread(test_img_filenames[i]));
+        test_img.test_name = calculate_filename(test_img_filenames[i]);
+        test_image_data.push_back(test_img);
     }
 
-    int total_pixels = image[0].train_img.cols * image[0].train_img.rows;
-    cout << total_pixels << endl;
+
 
     //new image vector for distances, this is used so the vector can be cleared after interations to insure that 1 images distances don't bleed into other images data
-    vector<Images>distance;
+    Images distance_img;
     //interates through the test data and then the train data for that- calculates distance then the knn before clearing the vector
-    for (int i = 0; i < (read_count_test); i++) {
-        unsigned char* test_grey = calculate_grey_scale_test(ref(test_imgs), i);
-        for (int j = 0; j < read_count; j++) {
-            unsigned char* train_grey = calculate_grey_scale_train(ref(image), j);
-            calculate_distance(test_grey, train_grey, 0, total_pixels, ref(distance), image[j].label);
-            delete[] train_grey;
+    for (int i = 0; i < test_image_data.size(); i++) {
+
+        //storing the current testing image as variable for easy access
+        auto test_image = test_image_data[i].test_img;
+
+        //allocating memory for the test_input and train_outputs
+        unsigned char* test_input = (unsigned char*)test_image.data;
+        unsigned char* test_output = new unsigned char[test_image.size().width * test_image.size().height];
+        //count for the max number of pixels
+        const int total_number_of_pixels = test_image.rows * test_image.cols * test_image.channels();
+        const int train_size = train_image_data.size();
+        convert_to_grayscale(test_input, test_output, 0, total_number_of_pixels, test_image.channels());
+
+
+
+        for (int j = 0; j < train_size; j++) {
+
+            //storing the current training image as variable for easy access
+            auto train_img = train_image_data[j].train_img;
+
+            //allocating memory for the train_input and train_outputs
+            unsigned char* train_input = (unsigned char*)train_img.data;
+            unsigned char* train_output = new unsigned char[train_img.size().width * train_img.size().height];
+
+            convert_to_grayscale(train_input, train_output, 0, total_number_of_pixels, train_img.channels());
+
+
+            //calculating the distance, then storing with: test_name and labels
+            distance_img.distance = calculate_distance(test_output, train_output, 0, (total_number_of_pixels / 3));
+            distance_img.label = train_image_data[j].label;
+            distance_img.test_name = test_image_data[i].test_name;
+            distance_data.push_back(distance_img);
+
+
         }
-        sort(distance.begin(), distance.end(), [](Images a, Images b) {return a.distance < b.distance; });
-        t_print(distance);
-        calculate_knn(ref(distance), 61);
-        delete[] test_grey;
-        distance.clear();
+
+        //function to simply sort the values in the image vector strcuture
+        sort(distance_data.begin(), distance_data.end(), [](Images a, Images b) {return a.distance < b.distance; });
+        //t_print(distance);
+        //call to calculate knn using the sorted values
+        calculate_knn(ref(distance_data), stoi(k_value));
+        //cleared vector to keep processing time down and to remove potenial bias
+        distance_data.clear();
     }
 
+    //ending timing
     auto end = steady_clock::now();
-    auto duration_s = duration_cast<microseconds> (end - start).count();
-    cout << "Parallel time: " << duration_s << endl;
+    //calculating duration
+    auto duration_p = duration_cast<seconds> (end - start).count();
+    cout << "----------------------------------------------------------" << endl;
+    //outputing parallel time
+    cout << "Parallel time: " << duration_p << endl;
 
     return 0;
 }
